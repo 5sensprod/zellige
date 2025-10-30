ec6f8d7c1acd758ff8d6e71fdd245cb2f0504bb5
chris <5sensprod@gmail.com>
2025-10-30 21:27:18 +0100

    Refacto css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 
-import "./styles/base.css";
+import "./styles/styles.css";
 
 export default function App() {
   return (
